4df2157554e2d975af7c0aa0f92b02ef5b9f596c
sambok23 <a24746440@gmail.com>
2024-01-16 16:11:07 +0900

    js init

---
+++ express-server/index.js
@@ -0,0 +1,41 @@
+import { createClient } from "redis"
+import express from "express"
+
+const client = createClient ({
+  url : "redis://default:72c6a1c60d694eb8b49f464368e9e7e0@apn1-immune-mongrel-34253.upstash.io:34253"
+});
+
+client.on("error", function(err) {
+    throw err;
+});
+client.connect().then();
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+const port = 3000
+
+app.get('/', async(req, res) => {
+    const cache = await client.get("posts");
+    if(cache) return res.json(JSON.parse(cache))
+    const result = await fetch('https://jsonplaceholder.typicode.com/posts/1')
+    const json = await result.json();
+    await client.set("posts", JSON.stringify(json));
+    res.send(json)
+})
+
+app.get("/posts/:id", async(req, res) => {
+    const id = req.params.id;
+    if(!id) res.sendStatus(400);
+    const cache = await client.get(`v1@posts/${id}`);
+    if(cache) return res.json(JSON.parse(cache))
+    const result = await fetch('https://jsonplaceholder.typicode.com/posts/1')
+    const json = await result.json();
+    await client.set(`v1@posts/${id}`, JSON.stringify(json));
+
+    res.send(json)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
